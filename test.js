591980e19d6e4024d3eaed8c7087980903c40362
sungtaekoh <ohbass95@gmail.com>
2021-10-13 16:52:17 +0900

    javascriptDay01

---
+++ test.js
@@ -0,0 +1,15 @@
+function test(){
+		alert('경고장')
+		if(num==1234){
+			alert('1234')
+		}
+		
+	}
+	/*
+	alert('경고장')
+		if(num==1234){
+			alert('1234')
+			이 기능이 없어도 정상적으로 작동한다
+			
+			function이 뭘까?
+	*/ 
